c2a382a25065eaa11a57677fec7aec0c0f09b4a6
fsuarez6 <fsuarez6@gmail.com>
2017-11-13 14:46:07 +0800

    Initial import

---
+++ .ci_preparation.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Install gclib: http://www.galilmc.com/sw/pub/all/doc/gclib/html/ubuntu.html
+# Create a temporary variable for Ubuntu version
+UVER=$(lsb_release -r | cut -f 2);
+# Install Galil's public certificate
+wget http://www.galil.com/sw/pub/ubuntu/$UVER/GALIL-PUB-KEY
+apt-key add GALIL-PUB-KEY
+# Get Galil's apt sources list
+wget http://www.galil.com/sw/pub/ubuntu/$UVER/galil.list -O /etc/apt/sources.list.d/galil.list
+apt-get update
+# Install gclib
+apt-get install gclib
+# Install the gclib Python wrapper
+mkdir ~/gclib_wrapper
+cd ~/gclib_wrapper
+tar -xvf /usr/share/doc/gclib/src/gclib_python.tar.gz
+python setup.py install --user
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # galilmc
+[![Build Status](https://travis-ci.org/crigroup/galilmc.svg?branch=master)](https://travis-ci.org/crigroup/galilmc)
+
 ROS package for Galil controllers
+
+This package has been developed by [CRI Group](http://www.ntu.edu.sg/home/cuong/),
+[Nanyang Technological University, Singapore](http://www.ntu.edu.sg).
+
+### Maintainer
+* [Francisco Suárez Ruiz](http://fsuarez6.github.io)
+
+### Documentation
+* Throughout the various files in this repository.
+* Website: http://wiki.ros.org/galilmc
